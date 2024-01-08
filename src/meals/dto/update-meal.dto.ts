eb22901b089186c1cfcc24184501e030d2cd5249
Eba Adisu <ebaadisu2@gmail.com>
2024-01-08 09:33:35 +0300

    Meals Create and Update Dto

--- src/meals/dto/update-meal.dto.ts
+++ src/meals/dto/update-meal.dto.ts
@@ -1,4 +1,71 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMealDto } from './create-meal.dto';
-
-export class UpdateMealDto extends PartialType(CreateMealDto) {}
+import {
+    IsNotEmpty,
+    IsString,
+    IsNumber,
+    Length,
+    IsArray,
+    IsIn,
+    IsBoolean,
+    IsOptional,
+  } from 'class-validator';
+  
+  export class UpdateMealDto {
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    name: string;
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsNumber(
+      { allowInfinity: false, allowNaN: false },
+      { message: 'Value must be a number.' },
+    )
+    price: number;
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    @Length(10, 200, {
+      message: 'Description must be between 10 and 200 characters long.',
+    })
+    description: string;
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    imageUrl: string;
+  
+    // taking: can we just use the isin option only
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsArray({ message: 'Value must be an array.' })
+    @IsIn(['Breakfast', 'Lunch', 'Dinner', 'Starter', 'Desert'], {
+      each: true,
+      message: 'Invalid Meals types.',
+    })
+    types: string[];
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsBoolean()
+    fasting: boolean;
+  
+    // taking: can we just use the isin option only
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsArray({ message: 'Value must be an array.' })
+    @IsIn(['Gluten', 'Lactose', 'Nuts', 'Shellfish', 'None'], {
+      each: true,
+      message: 'Invalid allergy type.',
+    })
+    allergies: string[];
+  
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    origin: string;
+  }
+  
