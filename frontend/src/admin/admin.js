d29c5fdb604153ef07660c483e79b7725061a0f0
chachidani <141312783+chachidani@users.noreply.github.com>
2024-01-12 10:39:20 -0900

    minor fix on add_food UI

--- frontend/src/admin/admin.js
+++ frontend/src/admin/admin.js
@@ -4,7 +4,8 @@ document.getElementById('foodForm').addEventListener('submit',  function (event)
     const pictureInput = document.getElementById('picture');
     const titleInput = document.getElementById('title');
     const priceInput = document.getElementById('price');
-    const kindSelect = document.getElementById('kindSelect');
+    const kindSelect = document.querySelectorAll('#kindSelect input[type="checkbox"]:checked');
+    // const checkboxes = document.querySelectorAll('#kindSelect input[type="checkbox"]:checked');
     const orginSelect = document.getElementById('orginSelect');
     const fastingSelect = document.getElementById('fastingSelect');
     const allergiesSelect = document.getElementById('allergiesSelect');
@@ -13,9 +14,10 @@ document.getElementById('foodForm').addEventListener('submit',  function (event)
     const newItem = {
         name: titleInput.value,
         price: parseFloat(priceInput.value),
-        description: description.value, // You may add a description field in your form
-        imageUrl: "../path/imgurl", // Replace with your image URL
-        types: Array.from(kindSelect.selectedOptions, option => option.value),
+        description: description.value, 
+        imageUrl: "../path/imgurl", 
+        types:  Array.from(kindSelect).map(checkbox => checkbox.value),
+        
         fasting: isFasting,
         allergies: allergiesSelect.value,
         origin: orginSelect.value,
